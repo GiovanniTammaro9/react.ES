a7b275f216c229543af328b6e92383f5e9f41899
GiovanniTammaro9 <gio_play@live.it>
2025-01-14 14:42:22 +0100

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Textinput from "./Textinput";
 import LoginForm from "./LoginForm";
 import Uncontrolledinput from "./Uncontrolledinput";
 import ItemList from "./ItemList";
+import Card from "./Card";
 
 function App() {
   const items = [`Mela`, `Banana`, `Pera`, `Arancia`];
@@ -17,6 +18,9 @@ function App() {
       <LoginForm />
       <Uncontrolledinput />
       <ItemList items={items} />
+      <Card>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo labore provident sint eius ratione repudiandae ea in facere illum id, neque a blanditiis! Illo doloremque repudiandae totam reprehenderit neque.</p>
+      </Card>
     </div>
     
   );
