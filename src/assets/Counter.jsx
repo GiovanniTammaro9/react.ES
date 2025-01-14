26fab704dd97d67774fc7aff4385de6abe8baa2d
GiovanniTammaro9 <gio_play@live.it>
2025-01-14 12:20:09 +0100

    useeffect

--- src/assets/Counter.jsx
+++ src/assets/Counter.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 function Counter() {
     const [count, setCount] = useState(0);
 
@@ -7,8 +7,13 @@ function Counter() {
     };
     const decrement = () => setCount(count - 1);
     const reset = () => setCount(0);
+
+    useEffect(() => {
+        document.title = `Contatore: ${count}`;
+    })
     return (
         <div>
+            <h1>Contatore: {count}</h1>
             <p>count: {count}</p>
             <button onClick={increment}>Incremento</button>
             <button onClick={decrement}>decrementa</button>
