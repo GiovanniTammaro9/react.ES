456b88c347a426357441287458a14ac2ec8e637e
GiovanniTammaro9 <gio_play@live.it>
2025-01-14 14:31:21 +0100

    itemlist

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import Counter from './assets/Counter';
 import Textinput from "./Textinput";
 import LoginForm from "./LoginForm";
 import Uncontrolledinput from "./Uncontrolledinput";
+import ItemList from "./ItemList";
 
 function App() {
+  const items = [`Mela`, `Banana`, `Pera`, `Arancia`];
+
+
   return (
     <div>
       <Textinput />
       <Counter />
       <LoginForm />
       <Uncontrolledinput />
+      <ItemList items={items} />
     </div>
     
   );
