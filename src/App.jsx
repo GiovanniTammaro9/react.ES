d57a18885863d78ae64f6ee5d617a9a3099d8440
GiovanniTammaro9 <gio_play@live.it>
2025-02-07 13:04:24 +0100

    aggiunto il todoProvider in app

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ItemList from "./ItemList";
 import Card from "./Card";
 import useFetch from "./hooks/useFetch";
 import TodoList from "./TodoList";
+import { TodoProvider } from "./TodoContext";
 
 function App() {
   const items = [`Mela`, `Banana`, `Pera`, `Arancia`];
@@ -15,20 +16,22 @@ function App() {
 
   return (
     <div>
-      <Textinput />
-      <Counter />
-      <LoginForm />
-      <Uncontrolledinput />
-      <ItemList items={items} />
-      <Card>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo labore provident sint eius ratione repudiandae ea in facere illum id, neque a blanditiis! Illo doloremque repudiandae totam reprehenderit neque.</p>
-      </Card>
-      <TodoList />
+      <TodoProvider>
+        <Textinput />
+        <Counter />
+        <LoginForm />
+        <Uncontrolledinput />
+        <ItemList items={items} />
+        <Card>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo labore provident sint eius ratione repudiandae ea in facere illum id, neque a blanditiis! Illo doloremque repudiandae totam reprehenderit neque.</p>
+        </Card>
+        <TodoList />
+      </TodoProvider>
     </div>
-    
+
   );
 
-  
+
 }
 
 
